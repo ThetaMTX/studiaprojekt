e201fa49c7c307556f7c818b2bebd43f3f53f52c
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2023-04-12 08:29:34 +0200

    changed settings

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -9,18 +9,23 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-qq7i2%w+$$t%&cyy0s)9zjo=9cd7q(jp*)^6xeadf@#!*ngy)r'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
